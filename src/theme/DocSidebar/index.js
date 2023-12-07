b2c793e4092a4e1a6ad672aba9d9c1417bd7a4ad
Ethan Rose <erose@apache.org>
2023-12-06 19:18:04 -0800

    Add colon to version dropdown

--- src/theme/DocSidebar/index.js
+++ src/theme/DocSidebar/index.js
@@ -7,7 +7,7 @@ export default function DocSidebarWrapper(props) {
       <>
         <div className={"scroll-custom thin-scrollbar"}>
         <div className="custom-sidebar-version">
-          <div className={"custom-sidebar-inner"}><span style={{ "display": "inline-block" }}>Version</span> <DocsVersionDropdownNavbarItem dropdownItemsBefore={[]} dropdownItemsAfter={[]} /></div>
+          <div className={"custom-sidebar-inner"}><span style={{ "display": "inline-block" }}>Version:</span> <DocsVersionDropdownNavbarItem dropdownItemsBefore={[]} dropdownItemsAfter={[]} /></div>
         </div>
         <div id="sidebarCssSelector">
           <DocSidebar {...props} />
