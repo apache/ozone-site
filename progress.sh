593c0f429c10585b890789025922a539c42572e1
Ethan Rose <erose@apache.org>
2024-03-21 17:28:18 -0700

    Automatically add missing license headers with skywalking eyes.

--- progress.sh
+++ progress.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env sh
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 
 # Run this script to see how many pages are filled in and how many are left. This is an approximation based on the TODO
 # messages in existing stubbed out pages. Pass -v or --verbose to get a specific list of pages.
