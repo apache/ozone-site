3121b38d5c12e89665a374bce9a05a2ae479c3bd
Wei-Chiu Chuang <weichiu@apache.org>
2026-02-19 00:31:35 -0800

    feat: Rename release-notes directory to release and update all references

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -358,6 +358,10 @@ const config = {
           //   label: 'FAQ',
           // },
           {
+            to: '/release',
+            label: 'Release Notes',
+          },
+          {
             to: '/blog',
             label: 'Blog',
           },
