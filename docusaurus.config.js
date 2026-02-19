88461e5008dd9ecdf8c176ff9886ae44aa08ca36
Wei-Chiu Chuang <weichiu@apache.org>
2026-02-19 10:53:40 -0800

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -358,10 +358,6 @@ const config = {
           //   label: 'FAQ',
           // },
           {
-            to: '/release',
-            label: 'Release Notes',
-          },
-          {
             to: '/blog',
             label: 'Blog',
           },
--- src/pages/release/index.md
+++ src/pages/release/index.md
@@ -3,6 +3,4 @@ title: Release Notes
 sidebar_label: Release Notes
 ---
 
-# Release Notes
-
 This page contains the release notes for Apache Ozone. Please navigate to the specific version to view its release notes.
